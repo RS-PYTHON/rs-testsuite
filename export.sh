1903d2ff95c51db26970f9af2be7aece3b2f5220
Vincent Privat <vincent.privat@airbus.com>
2024-09-02 11:08:43 +0200

    Restrict JIRA import and badge update to ENV_OPS

--- export.sh
+++ export.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+BUNDLE="$HOME/snap/firefox/common/tmp/downloads/FeatureBundle.zip"
+
 pip install -r requirements.txt && \
+rm -f $BUNDLE && \
 python3 jira_export_features.py && \
-unzip -u -o -d features ~/snap/firefox/common/tmp/downloads/FeatureBundle.zip && \
+rm -f features/*.feature && \
+unzip -u -o -d features $BUNDLE && \
+rm -f $BUNDLE && \
 ls -l features
 
