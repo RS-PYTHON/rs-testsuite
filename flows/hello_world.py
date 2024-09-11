16523389b864954ae7eabf991801e3c527c286bd
pcuq-ads <87175027+pcuq-ads@users.noreply.github.com>
2024-09-11 15:49:25 +0200

    Revert hello_world.py

--- flows/hello_world.py
+++ flows/hello_world.py
@@ -1,15 +1,6 @@
-from prefect import flow, task
-from prefect.artifacts import create_markdown_artifact
+from prefect import flow
 
-@task
-def generate_report() -> str:
-    report = "# Rapport de flux\n test Hello World"
-    return report
 
 @flow
 def my_flow() -> str:
-    create_markdown_artifact(key="rapport", markdown=generate_report())
     return "Hello, world!"
-
-if __name__ == "__main__":
-    my_flow()
