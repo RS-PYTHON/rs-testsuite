ac0d704a9b5e9338e44badbf82b280a1e7c33d65
pcuq-ads <87175027+pcuq-ads@users.noreply.github.com>
2024-09-11 15:11:13 +0200

    Update hello_world.py

--- flows/hello_world.py
+++ flows/hello_world.py
@@ -1,4 +1,4 @@
-from prefect import flow
+from prefect import flow, task
 from prefect.artifacts import create_markdown_artifact
 
 @task
@@ -8,6 +8,9 @@ def generate_report():
 
 @flow
 def my_flow() -> str:
-    report = generate_report()
+    report = generate_report().result()  # Ajout de .result() pour obtenir le résultat de la tâche
     create_markdown_artifact(key="rapport", markdown=report)
     return "Hello, world!"
+
+if __name__ == "__main__":
+    my_flow()
