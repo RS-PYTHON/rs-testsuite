b978a2fea1dbeb0ac19ac6678b1d26c195378adc
Pierre CUQ <pierre.cuq@airbus.com>
2024-09-11 15:50:07 +0200

    add first flow

--- flows/hello_world.py
+++ flows/hello_world.py
@@ -1,6 +1,17 @@
-from prefect import flow
+from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
 
+@task
+def generate_report() -> str:
+    report = "# Rapport de flux\n test Hello World"
+    return report
 
 @flow
 def my_flow() -> str:
+    create_markdown_artifact(key="rapport", markdown=generate_report())
     return "Hello, world!"
+
+if __name__ == "__main__":
+    my_flow() 
+    
+    
