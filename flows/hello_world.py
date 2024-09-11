5a5a9187f1719430aa7f7bd194e25951a5d7651f
Pierre CUQ <pierre.cuq@airbus.com>
2024-09-11 15:57:01 +0200

    update hello world flow

--- flows/hello_world.py
+++ flows/hello_world.py
@@ -3,15 +3,17 @@ from prefect.artifacts import create_markdown_artifact
 
 @task
 def generate_report() -> str:
-    report = "# Rapport de flux\n test Hello World"
+    report = "# Rapport de flux\n test Hello World \n yes "
+    report += "AAAA \n "
+    report += "BBB \n "
     return report
 
 @flow
-def my_flow() -> str:
-    create_markdown_artifact(key="rapport", markdown=generate_report())
+def my_flow5() -> str:
+    create_markdown_artifact(key="rapport5", markdown=generate_report())
     return "Hello, world!"
 
 if __name__ == "__main__":
-    my_flow() 
+    my_flow5() 
     
     
