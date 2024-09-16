bc68060fc7d71575b8f2d4acdb3c2dcdad449de8
Pierre CUQ <pierre.cuq@airbus.com>
2024-09-16 13:54:36 +0200

    update header

--- features/steps/catalog.py
+++ features/steps/catalog.py
@@ -1,14 +1,11 @@
 """STAC Catalog test steps"""
 
 from behave import given, when, then
-from behave import use_step_matcher
 
 import os
 import requests
 import uuid
 import json
-from bs4 import BeautifulSoup
-from datetime import datetime, timezone
 from urllib.parse import urljoin
 
 
