503ecc36f66b64d4a1ab86e93273cc63405fd2de
Pierre CUQ <pierre.cuq@airbus.com>
2024-09-17 10:17:54 +0200

    add 2 artifacts

--- flows/utils/artifacts.py
+++ flows/utils/artifacts.py
@@ -1,4 +1,4 @@
-from prefect.artifacts import create_table_artifact
+from prefect.artifacts import create_table_artifact, create_markdown_artifact
 # wait for v3 - from prefect.artifacts import create_progress_artifact, update_progress_artifact
 from datetime import datetime
 
@@ -44,6 +44,43 @@ class ReportManager:
 
         # Formater la date et l'heure en texte
         date_texte = now.strftime("%A %d %B %Y, %H:%M:%S")
+
+        na_revenue = 500000
+        markdown_report = f"""# Sales Report
+
+        ## Summary
+
+        In the past quarter, our company saw a significant increase in sales, with a total revenue of $1,000,000. 
+        This represents a 20% increase over the same period last year.
+
+        ## Sales by Region
+
+        | Region        | Revenue |
+        |:--------------|-------:|
+        | North America | ${na_revenue:,} |
+        | Europe        | $250,000 |
+        | Asia          | $150,000 |
+        | South America | $75,000 |
+        | Africa        | $25,000 |
+
+        ## Top Products
+
+        1. Product A - $300,000 in revenue
+        2. Product B - $200,000 in revenue
+        3. Product C - $150,000 in revenue
+
+        ## Conclusion
+
+        Overall, these results are very encouraging and demonstrate the success of our sales team in increasing revenue 
+        across all regions. However, we still have room for improvement and should focus on further increasing sales in 
+        the coming quarter.
+        """
+        create_markdown_artifact(
+                key=key_value.lower(),
+                markdown=markdown_report,
+                description="Quarterly Sales Report",
+        )
+
 
         return create_table_artifact(
             key=key_value.lower(),
