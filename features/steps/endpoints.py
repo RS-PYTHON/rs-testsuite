32abad860e3b6c8f74ce28dc38d9b2f14d79f3b0
Pierre CUQ <pierre.cuq@airbus.com>
2024-09-20 16:44:38 +0200

    add control of version

---
+++ features/steps/endpoints.py
@@ -0,0 +1,42 @@
+from behave import given, when, then
+
+import os
+import requests
+import json
+
+# Call Get on the endpoint "https://" + service + "." + RS_PYTHON_URL + "/" + path
+@when('the rs-python service {service} is requested with the path {path}')
+def step_request_service(context: str, service: str, path: str)->str:
+    # Ensure that OAuth2 authentication has been performed.
+    assert context.cookies is not None
+    
+    # Ensure that the RS-Python URL is defined.
+    assert os.getenv("RS_PYTHON_URL") is not None
+    
+    with requests.Session() as session:
+        session.cookies.update(context.cookies)
+
+        # Construct the endpoint URL
+        url = "https://" + service.lstrip('/') + "." + os.getenv("RS_PYTHON_URL").rstrip('/') + "/" + path.lstrip('/')
+        headers = {"Accept": "application/json"}
+        
+        # Send the GET request and get the JSON response
+        response = session.get(url, headers=headers)
+        response.raise_for_status()
+
+        # Store the response status code and the response itself
+        context.response_status_code = response.status_code
+        context.response = response
+        return response
+        
+# Check that the server responded with the specified status code.    
+@then('the server should answer with the code {code:d}')
+def step_request_code(context: str, code: int):
+    assert context.response_status_code == code
+
+def is_valid_json(chain: str):
+    try:
+        json.loads(chain)
+        return True
+    except:
+        return False
--- features/steps/prometheus.py
+++ features/steps/prometheus.py
@@ -1,76 +1,66 @@
 from behave import given, when, then
-
-import os
-import requests
+from behave import use_step_matcher
 import json
+import re
+from endpoints import step_request_service, is_valid_json
+
+container_tab = [
+    ('prefect',                 ('.*prefect-server.*',          '',                           ':(\d+\.\d+\.\d+)-')            ),
+    ('rs-server-frontend',      ('.*rs-server-frontend.*',      '',                           ':([0-9a-zA-Z.]+)$')   ),
+    ('rs-server-adgs',          ('.*rs-server-adgs.*',          '',                           ':([0-9a-zA-Z.]+)$')   ),
+    ('rs-server-cadip',         ('.*rs-server-cadip.*',         '',                           ':([0-9a-zA-Z.]+)$')   ),
+    ('rs-server-staging',       ('.*rs-server-staging.*',       '',                           ':([0-9a-zA-Z.]+)$')   ),
+    ('rs-server-catalog',       ('.*server-catalog.*',          '.*server-catalog-db.*',      ':([0-9a-zA-Z.]+)$')   ),
+    ('pgstac',                  ('.*server-catalog-db.*',       '',                           'v(\d+\.\d+\.\d+)')   )
+    ]
 
 
+
+# Call Prometheus query
 @when('we call the prometheus query {query}')
 def step_request_prometheus(context: str, query: str):
-    """
-    Call Prometheus query
-    """
     step_request_service(context, 'monitoring', 'prometheus/api/v1/query?query=' + query)
 
+
+# Check that the Prometheus query send back almost one item
 @then('almost one prometheus result is provided')
 def step_check_prometheus_result(context: str):
-    """
-    Check Prometheus response
-    """
     step_check_json_prometheus_is_not_null(context, 'data', 'result')
 
-@when('the rs-python service {service} is requested with the path {path}')
-def step_request_service(context: str, service: str, path: str):
-    """
-    This step calls the endpoint "https://" + service + "." + RS_PYTHON_URL + "/" + path
-    and records the status code.
-    """
-    # Ensure that OAuth2 authentication has been performed.
-    assert context.cookies is not None
-    
-    # Ensure that the RS-Python URL is defined.
-    assert os.getenv("RS_PYTHON_URL") is not None
-    
-    with requests.Session() as session:
-        session.cookies.update(context.cookies)
 
-        # Construct the endpoint URL
-        url = "https://" + service.lstrip('/') + "." + os.getenv("RS_PYTHON_URL").rstrip('/') + "/" + path.lstrip('/')
-        print(f"url {url}")
-        
-        headers = {
-            "Accept": "application/json"
-        }
-        
-        # Send the GET request and get the JSON response
-        response = session.get(url, headers=headers)
-        response.raise_for_status()
 
-        # Store the response status code and the response itself
-        context.response_status_code = response.status_code
-        context.response = response
-
-@then('the server should answer with the code {code:d}')
-def step_request_code(context: str, code: int):
-    """
-    Check that the server responded with the specified status code.
-    """
-    assert context.response_status_code == code
-
-def is_valid_json(chain: str):
-    try:
-        json.loads(chain)
-        return True
-    except:
-        return False
-
+# Specific check: Ensure the response is in JSON format and that the path level1.level2 contains at least one element.
 @then('the answer is a json with almost one element on the path {level1}.{level2}')
 def step_check_json_prometheus_is_not_null(context: str, level1: str, level2: str):
-    """
-    Specific check: Ensure the response is in JSON format and that the path level1.level2 contains at least one element.
-    """
     assert context.response_status_code == 200
     assert is_valid_json(context.response.text) == True
 
     data = json.loads(context.response.text)
     assert len(data[level1][level2]) > 0
+
+
+use_step_matcher("re")
+
+# Check the version of a specific container
+@given ('the container (?P<container>prefect|rs-server-frontend|rs-server-adgs|rs-server-cadip|rs-server-staging|rs-server-catalog|pgstac) has got version (?P<version>[^"]+)')
+@then  ('the container (?P<container>prefect|rs-server-frontend|rs-server-adgs|rs-server-cadip|rs-server-staging|rs-server-catalog|pgstac) has got version (?P<version>[^"]+)')
+def step_check_container_version(context: str, container: str, version: str):
+    # Retrieve the configuration for the specified container from container_tab
+    configuration = next((valeurs for key, valeurs in container_tab if key == container), None)
+    
+    # Get container information from Prometheus
+    query = f'kube_pod_container_info{{  pod=~"{configuration[0]}", pod!~"{configuration[1]}"  }}'
+    response = step_request_service(context, 'monitoring', 'prometheus/api/v1/query?query=' + query)
+
+    # Get the image value from the response data
+    data = json.loads(response.text)
+    image_value = (data['data']['result'][0]['metric']['image'])
+    
+    # Use regex to find the version in the image value
+    match = re.search(fr'{configuration[2]}', image_value)
+    version_extracted = match.group(1)
+    
+    # Assert that the extracted version matches the expected version
+    assert (version_extracted == version)
+
+
