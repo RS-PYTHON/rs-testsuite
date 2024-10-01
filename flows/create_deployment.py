cdb6dc55ece5d40f528530f5657e32c97a543c8c
Vincent Privat <vincent.privat@airbus.com>
2024-10-01 16:34:30 +0200

    fix more flake8 errors

--- flows/create_deployment.py
+++ flows/create_deployment.py
@@ -2,14 +2,16 @@ from prefect import flow
 from prefect.runner.storage import GitRepository
 import sys
 
+
 # This function must be called within the cluster. From JupytherLab for example.
-def deploy_flow(entrypoint:str, deployment:str, workpool:str="on-demand-k8s-pool", branch:str="feature/endpoint", github_url:str="https://github.com/RS-PYTHON/rs-testsuite.git"): 
+def deploy_flow(entrypoint: str, deployment: str, workpool: str = "on-demand-k8s-pool", branch: str = "feature/endpoint",
+                github_url: str = "https://github.com/RS-PYTHON/rs-testsuite.git"):
     flow.from_source(
         source=GitRepository(
             url=github_url,
             branch=branch,
         ),
-    entrypoint=entrypoint       
+        entrypoint=entrypoint
     ).deploy(
         name=deployment,
         work_pool_name=workpool
@@ -17,14 +19,12 @@ def deploy_flow(entrypoint:str, deployment:str, workpool:str="on-demand-k8s-pool
 
 
 if __name__ == "__main__":
-    if ( (len(sys.argv) < 3) or (len(sys.argv) > 6)  ):
+    if ((len(sys.argv) < 3) or (len(sys.argv) > 6)):
         print("Usage: python create_deployment.py <entrypoint> <deployment> [<workpool>] [<branch>] [<github_url>]")
     else:
         deploy_flow(*sys.argv[1:])
 
 """
-   Sample: 
+   Sample:
     python create_deployment.py flows/flow_template.py:flow_template flow-template-deployment
-
-
 """
--- flows/flow_template.py
+++ flows/flow_template.py
@@ -8,19 +8,22 @@ report_manager = ReportManager(4)
 def step1():
     report_manager.success_step(1, "Step1 description")
 
+
 @task
 def step2():
     report_manager.success_step(2, "Step2 description")
-    
+
+
 @task
 def step3():
     report_manager.failed_step(3, "Step3 description")
-    
+
+
 @task
 def step4():
     report_manager.success_step(4, "Step4 description")
-    
-    
+
+
 @flow
 def flow_template() -> str:
     # Start the 4 tasks in parallel
@@ -28,8 +31,7 @@ def flow_template() -> str:
     future2 = step2.submit()
     future3 = step3.submit()
     future4 = step4.submit()
-    
-    
+
     # Wait all of them to finish
     future1.wait()
     future2.wait()
@@ -38,12 +40,9 @@ def flow_template() -> str:
 
     # To start tasks in sequence, remove submit() and remove wait()
 
-    
-    report_manager.add_report_as_artefact("flow-template-test", "Template" )
+    report_manager.add_report_as_artefact("flow-template-test", "Template")
     return "This is a flow template"
 
 
 if __name__ == "__main__":
-    flow_template() 
-    
-    
+    flow_template()
