3f5b546efd56bb36986d2057f1761b239890b6a0
pcuq-ads <pierre.cuq@airbus.com>
2025-05-16 17:20:29 +0200

    update aio parameters

--- flows/systematic/flow_s1_aio.py
+++ flows/systematic/flow_s1_aio.py
@@ -8,7 +8,7 @@ report_manager = ReportManager(2)
 
 @task(name="launch-connection", description="Connect to the station")
 def s1_aio(station: str, session_id: str):
-    report_manager.success_step(1, f"Start generic processing with S1-AIO on session name {session_id}")
+    report_manager.success_step(1, f"Start generic processing with S1-AIO on session name {session_id} on station {station}")
     time.sleep(1)
     send_event(2, station, session_id)
 
@@ -29,7 +29,7 @@ def send_event(step, station, session_id):
 
 @flow
 def s1_aio_start(station: str, session_id: str):
-    s1_aio(session_id)
+    s1_aio(station, session_id)
 
 if __name__ == "__main__":
     s1_aio_start("fake_station", "fake_session_name")
