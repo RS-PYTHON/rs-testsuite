60d0fde5ef601c0a266fcb1da2470c55f00317e9
pcuq-ads <pierre.cuq@airbus.com>
2025-05-16 12:21:13 +0200

    update artifacts

--- flows/systematic/flow_retrieve_s1_session.py
+++ flows/systematic/flow_retrieve_s1_session.py
@@ -1,4 +1,5 @@
 from prefect import flow, task
+from datetime import datetime, timedelta
 from flows.utils.artifacts import ReportManager
 
 report_manager = ReportManager(2)
@@ -6,47 +7,30 @@ report_manager = ReportManager(2)
 
 @task(name="station-connection", description="Connect to the station")
 def connect():
-    report_manager.success_step(1, "Step1 description")
+    report_manager.success_step(1, "Connect to station")
 
 @task(name="get-session", description="Retrieve the list of sessions between two dates")
-def retrieve_session():
-    report_manager.success_step(2, "Step2 description")
+def retrieve_session(to, tf):
+    report_manager.success_step(2, f"Retrieve sessions between {to} and {tf}")
 
 @flow
-def retrieve_s1_list_of_sessions() -> str:
-    markdown_report = """# Sales Report
+def retrieve_s1_list_of_sessions(delta_time_seconds: int) -> str:
+    now = datetime.now()
+    before = now - timedelta(seconds=delta_time_seconds)
 
+    markdown_report = f"""# Sales Report
 ## Summary
 
-In the past quarter, our company saw a significant increase in sales, with a total revenue of $1,000,000. 
-This represents a 20% increase over the same period last year.
+This flow will retrieve sentinel-1 sessions from stations between two dates :
+- **Start date**: {before.strftime("%Y-%m-%d %H:%M:%S")}
+- **End date**: {now.strftime("%Y-%m-%d %H:%M:%S")}
 
-## Sales by Region
-
-| Region        | Revenue |
-|:--------------|-------:|
-| Europe        | $250,000 |
-| Asia          | $150,000 |
-| South America | $75,000 |
-| Africa        | $25,000 |
-
-## Top Products
-
-1. Product A - $300,000 in revenue
-2. Product B - $200,000 in revenue
-3. Product C - $150,000 in revenue
-
-## Conclusion
-
-Overall, these results are very encouraging and demonstrate the success of our sales team in increasing revenue
-across all regions. However, we still have room for improvement and should focus on further increasing sales in
-the coming quarter.
 """
     report_manager.add_markdown_as_artefact("summary", markdown_report, "my description")
 
     # Start the 2 tasks in sequence
     connect()
-    retrieve_session()
+    retrieve_session(before, now)
 
     report_manager.add_report_as_artefact("retrieve-sentinel1-sessions", "Template")
     return "This is a flow template"
