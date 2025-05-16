1a837ca2172c29d6ec578e623871cd68adaac019
pcuq-ads <pierre.cuq@airbus.com>
2025-05-16 12:26:12 +0200

    fix flake

--- flows/systematic/flow_retrieve_s1_session.py
+++ flows/systematic/flow_retrieve_s1_session.py
@@ -9,17 +9,18 @@ report_manager = ReportManager(2)
 def connect():
     report_manager.success_step(1, "Connect to station")
 
+
 @task(name="get-session", description="Retrieve the list of sessions between two dates")
 def retrieve_session(to, tf):
     report_manager.success_step(2, f"Retrieve sessions between {to} and {tf}")
 
+
 @flow
-def retrieve_s1_list_of_sessions(delta_time_seconds: int) -> str:
+def retrieve_s1_list_of_sessions(delta_time_seconds: int):
     now = datetime.now()
     before = now - timedelta(seconds=delta_time_seconds)
 
-    markdown_report = f"""# Sales Report
-## Summary
+    markdown_report = f"""## Summary
 
 This flow will retrieve sentinel-1 sessions from stations between two dates :
 - **Start date**: {before.strftime("%Y-%m-%d %H:%M:%S")}
@@ -32,8 +33,7 @@ This flow will retrieve sentinel-1 sessions from stations between two dates :
     connect()
     retrieve_session(before, now)
 
-    report_manager.add_report_as_artefact("retrieve-sentinel1-sessions", "Template")
-    return "This is a flow template"
+    report_manager.add_report_as_artefact("retrieve-sentinel1-sessions", "retrieve sentinel-1 sessions")
 
 
 if __name__ == "__main__":
