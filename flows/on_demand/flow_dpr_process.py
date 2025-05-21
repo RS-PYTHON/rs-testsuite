129689734d7b7e31aab46c55bf17dc566fd753d9
pcuq-ads <pierre.cuq@airbus.com>
2025-05-21 09:23:38 +0200

    fix: update info_input task to correct markdown formatting and use create_markdown_artifact for reporting

--- flows/on_demand/flow_dpr_process.py
+++ flows/on_demand/flow_dpr_process.py
@@ -3,6 +3,7 @@ from flows.utils.artifacts import ReportManager
 import time
 from enum import Enum
 from typing import List, Tuple
+from prefect.artifacts import create_markdown_artifact
 
 
 class ProcessorName(Enum):
@@ -19,19 +20,25 @@ report_manager = ReportManager(2)
 def info_input(input_product_list: list[str], processor_name: ProcessorName,
                processor_version: str, processing_unit: str, dask_cluster_id: str,
                aux_collection: List[Tuple[str, str]], output_product_collection: List[Tuple[str, str]]):
-    markdown_output = f"""
-    # Input parameters when calling the flow :
-    | Parameter                     | Value                         |
-    |-------------------------------|-------------------------------|
-    | **input_product_list**        | `{input_product_list}`        |
-    | **processor_name**            | `{processor_name}`            |
-    | **processor_version**         | `{processor_version}`         |
-    | **processing_unit**           | `{processing_unit}`           |
-    | **dask_cluster_id**           | `{dask_cluster_id}`           |
-    | **aux_collection**            | `{aux_collection}`            |
-    | **output_product_collection** | `{output_product_collection}` |
-    """
-    report_manager.add_markdown_as_artefact("input", markdown_output, "")
+    markdown_report = f"""# Input parmaters
+
+## Input parameters when calling the flow
+| Parameter                     | Value                         |
+|:------------------------------|-----------------------------:|
+| **input_product_list**        | `{input_product_list}`        |
+| **processor_name**            | `{processor_name}`            |
+| **processor_version**         | `{processor_version}`         |
+| **processing_unit**           | `{processing_unit}`           |
+| **dask_cluster_id**           | `{dask_cluster_id}`           |
+| **aux_collection**            | `{aux_collection}`            |
+| **output_product_collection** | `{output_product_collection}` |
+
+"""
+    create_markdown_artifact(
+        key="input-report",
+        markdown=markdown_report,
+        description="Input for DPR processing"
+    )
 
 
 @flow
