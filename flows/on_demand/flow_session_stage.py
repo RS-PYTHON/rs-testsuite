8b240f609a518fc47bfb49f932d179efe35339dd
pcuq-ads <pierre.cuq@airbus.com>
2025-05-21 16:35:29 +0200

    fix: update session_stage function signature to use Literal for mission parameter values

--- flows/on_demand/flow_session_stage.py
+++ flows/on_demand/flow_session_stage.py
@@ -44,7 +44,7 @@ def send_event(mission: Mission, station: Station, session_id: str):
 
         
 @flow (validate_parameters=True)
-def session_stage(mission: Literal[Mission.S1, Mission.S2, Mission.S3], station: Station, session_id: str):
+def session_stage(mission: Literal["s1", "s2", "s3"], station: Station, session_id: str):
     retrieve_all_cadus(session_id, station)
     send_event(mission=mission, station=station, session_id=session_id)
     report_manager.add_report_as_artefact("retrieve-sentinel1-sessions", "retrieve sentinel-1 sessions")
