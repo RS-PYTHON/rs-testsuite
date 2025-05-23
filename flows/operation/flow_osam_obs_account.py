48c805270c3da3a73f8b736cf6afa55397c3266d
pcuq-ads <pierre.cuq@airbus.com>
2025-05-23 10:55:53 +0200

    fix: change default value of account_name parameter to an empty string

--- flows/operation/flow_osam_obs_account.py
+++ flows/operation/flow_osam_obs_account.py
@@ -2,7 +2,7 @@ from prefect import flow
 
 
 @flow (log_prints=True, validate_parameters=True)
-def osam_obs_account(account_name: str = None):
+def osam_obs_account(account_name: str = ""):
     print("start osam obs account")
     
     
