a41627e00be53f23b44741ad7d19d161249e088e
pcuq-ads <pierre.cuq@airbus.com>
2025-07-03 14:44:27 +0200

    refactor: standardize function names and improve markdown descriptions in DPR and session stage flows

--- flows/on_demand/flow_session_stage.py
+++ flows/on_demand/flow_session_stage.py
@@ -4,7 +4,7 @@ from prefect.context import TaskRunContext
 from flows.utils.artifacts import ReportManager
 import time
 import random
-from  flows.utils.copernicus_enum import Station, Mission
+from flows.utils.copernicus_enum import Station, Mission
 from typing import Literal
 
 
@@ -12,7 +12,7 @@ report_manager = ReportManager(2)
 
 
 @task(description="Retrieve all CADU chunks from session {session_id}")
-def retrieve_all_cadus(session_id: str, station : Station):
+def retrieve_all_cadus(session_id: str, station: Station):
     task_run_ctx = TaskRunContext.get()
     task_run_ctx.task_run.name = f"Stage session {session_id} from station {station}"
 
@@ -42,8 +42,7 @@ def send_event(mission: str, station: str, session_id: str):
                payload=payload_json)
 
 
-        
-@flow (validate_parameters=True)
+@flow(validate_parameters=True)
 def session_stage(mission: Literal["s1", "s2", "s3"], 
                   station: Literal["sgs", "mti", "mps", "ins", "kse", "par", "nsg"],
                   session_id: str, emit_event: bool = False):
