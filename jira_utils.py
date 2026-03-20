83e3021cc8eed366eb770d34d01464fc8e3eea97
Vincent Privat <vincent.privat@airbus.com>
2026-03-20 11:12:25 +0100

    Fix JIRA login

--- jira_utils.py
+++ jira_utils.py
@@ -5,10 +5,15 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
+from typing import Any
 
 import geckodriver_autoinstaller
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, WebDriverException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException,
+    WebDriverException,
+)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
@@ -77,7 +82,12 @@ def firefox_browser() -> tuple[FirefoxProfile, webdriver.Firefox, str]:
     return firefox_profile, browser, download_dir
 
 
-def login_to_jira(browser: webdriver.Firefox, jira_url: str, login: str, password: str):
+def login_to_jira(  # noqa: C901
+    browser: webdriver.Firefox,
+    jira_url: str,
+    login: str,
+    password: str,
+):
     """Login to Jira/Xray using the provided credentials"""
     success = False
     for _ in range(5):
@@ -115,21 +125,39 @@ def login_to_jira(browser: webdriver.Firefox, jira_url: str, login: str, passwor
     WebDriverWait(browser, 30).until(
         EC.visibility_of_element_located((By.CLASS_NAME, "login-link")),
     )
-    browser.find_element(By.CLASS_NAME, "login-link").click()
 
-    try:
+    input_username, input_password, login_button = extract_login_form(browser)
+
+    if not input_username and not input_password and not login_button:
+        # Login form not displayed, happens sometimes ???
+        print(":: JIRA login form not displayed, requesting it")
+        browser.find_element(By.CLASS_NAME, "login-link").click()
         # JIRA login form
-        print(":: Waiting for JIRA login form")
-        WebDriverWait(browser, 30).until(
-            EC.element_to_be_clickable((By.ID, "username-field")),
-        )
-        browser.find_element(By.ID, "username-field").send_keys(login)
-        browser.find_element(By.ID, "password-field").send_keys(password)
-        browser.find_element(By.ID, "login-button").click()
-    except TimeoutException:
-        print(":: Jira login form not found, maybe already logged in?")
-        print(":: Title:", browser.title)
-        print(":: URL:", browser.current_url)
+        try:
+            print(":: Waiting for JIRA login form")
+            WebDriverWait(browser, 30).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        '//*[@id="username-field" or @id="login-form-username"]',
+                    ),
+                ),
+            )
+        except TimeoutException:
+            print(":: Jira login form not found")
+            print_context(browser)
+            raise
+        input_username, input_password, login_button = extract_login_form(browser)
+
+    if not input_username or not input_password or not login_button:
+        # Have some fields of the login form but not all of them => code to update ?
+        print(":: JIRA login form only partially found!")
+        print_context(browser)
+        raise OSError("Failed to extract JIRA login form")
+
+    input_username.send_keys(login)
+    input_password.send_keys(password)
+    login_button.click()
 
     try:
         print(":: Waiting for JIRA login success")
@@ -138,14 +166,54 @@ def login_to_jira(browser: webdriver.Firefox, jira_url: str, login: str, passwor
         )
     except TimeoutException:
         print(":: Jira login success not found")
-        print(":: Title:", browser.title)
-        print(":: URL:", browser.current_url)
-        print(":: HTML Page:")
-        print(browser.page_source)
+        print_context(browser)
         raise
 
 
-def get_cookies(browser: webdriver.Firefox):
+def extract_login_form(browser: webdriver.Firefox) -> tuple[Any, Any, Any]:
+    # JIRA has two distinct login forms !!!
+    return (
+        find_element_without_error(
+            browser,
+            By.ID,
+            "username-field",
+            "login-form-username",
+        ),
+        find_element_without_error(
+            browser,
+            By.ID,
+            "password-field",
+            "login-form-password",
+        ),
+        find_element_without_error(browser, By.ID, "login-button", "login-form-submit"),
+    )
+
+
+def find_element_without_error(
+    browser: webdriver.Firefox,
+    by: str,
+    value1: str,
+    value2: str,
+) -> Any | None:
+    try:
+        return browser.find_element(by, value1)
+    except NoSuchElementException as e1:
+        logging.debug(e1)
+        try:
+            return browser.find_element(by, value2)
+        except NoSuchElementException as e2:
+            logging.debug(e2)
+            return None
+
+
+def print_context(browser: webdriver.Firefox):
+    print(":: Title:", browser.title)
+    print(":: URL:", browser.current_url)
+    print(":: HTML Page:")
+    print(browser.page_source)
+
+
+def get_cookies(browser: webdriver.Firefox) -> dict:
     """Return the browser cookies"""
     cookies_dict = {}
     for cookie in browser.get_cookies():
